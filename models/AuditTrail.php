f4c4e7cabf162fc9438affb3fb15027005b22b07
Michal Krkavec <krkavec.michal@gmail.com>
2013-07-06 23:23:23 +0200

    refactoring
    mainly whitespace and some parentheses, removed setter and getter for
    oldAttributes

--- models/AuditTrail.php
+++ models/AuditTrail.php
@@ -3,8 +3,7 @@
 /**
  * This is the model class for table "{{audit_trail}}".
  */
-class AuditTrail extends CActiveRecord
-{
+class AuditTrail extends CActiveRecord {
 	/**
 	 * The followings are the available columns in table '{{audit_trail}}':
 	 * @var integer $id
@@ -22,27 +21,25 @@ class AuditTrail extends CActiveRecord
 	 * Returns the static model of the specified AR class.
 	 * @return AuditTrail the static model class
 	 */
-	public static function model($className=__CLASS__)
-	{
+	public static function model($className=__CLASS__) {
 		return parent::model($className);
 	}
 
 	/**
 	 * @return string the associated database table name
 	 */
-	public function tableName()
-	{
-		if ( isset(Yii::app()->params['AuditTrail']) && isset(Yii::app()->params['AuditTrail']['table']) )
+	public function tableName() {
+		if (isset(Yii::app()->params['AuditTrail']) && isset(Yii::app()->params['AuditTrail']['table'])) {
 		    return Yii::app()->params['AuditTrail']['table'];
-		else
+		} else {
 		    return '{{audit_trail}}';
+		}
 	}
 
 	/**
 	 * @return array validation rules for model attributes.
 	 */
-	public function rules()
-	{
+	public function rules() {
 		// NOTE: you should only define rules for those attributes that
 		// will receive user inputs.
 		return array(
@@ -61,8 +58,7 @@ class AuditTrail extends CActiveRecord
 	/**
 	 * @return array relational rules.
 	 */
-	public function relations()
-	{
+	public function relations()	{
 		// NOTE: you may need to adjust the relation name and the related
 		// class name for the relations automatically generated below.
 		return array(
@@ -73,8 +69,7 @@ class AuditTrail extends CActiveRecord
 	/**
 	 * @return array customized attribute labels (name=>label)
 	 */
-	public function attributeLabels()
-	{
+	public function attributeLabels() {
 		return array(
 			'id' => 'ID',
 			'old_value' => 'Old Value',
@@ -88,7 +83,7 @@ class AuditTrail extends CActiveRecord
 		);
 	}
 
-	function getParent(){
+	function getParent() {
 		$model_name = $this->model;
 		return $model_name::model();
 	}
@@ -97,20 +92,19 @@ class AuditTrail extends CActiveRecord
 	 * Retrieves a list of models based on the current search/filter conditions.
 	 * @return CActiveDataProvider the data provider that can return the models based on the search/filter conditions.
 	 */
-	public function search($options = array())
-	{
+	public function search($options = array()) {
 		// Warning: Please modify the following code to remove attributes that
 		// should not be searched.
 		$criteria=new CDbCriteria;
-		$criteria->compare('id',$this->id);
-		$criteria->compare('old_value',$this->old_value,true);
-		$criteria->compare('new_value',$this->new_value,true);
-		$criteria->compare('action',$this->action,true);
-		$criteria->compare('model',$this->model);
-		$criteria->compare('field',$this->field,true);
-		$criteria->compare('stamp',$this->stamp,true);
-		$criteria->compare('user_id',$this->user_id);
-		$criteria->compare('model_id',$this->model_id);
+		$criteria->compare('id', $this->id);
+		$criteria->compare('old_value', $this->old_value, true);
+		$criteria->compare('new_value', $this->new_value, true);
+		$criteria->compare('action', $this->action, true);
+		$criteria->compare('model', $this->model);
+		$criteria->compare('field', $this->field, true);
+		$criteria->compare('stamp', $this->stamp, true);
+		$criteria->compare('user_id', $this->user_id);
+		$criteria->compare('model_id', $this->model_id);
 		$criteria->mergeWith($this->getDbCriteria());
 		return new CActiveDataProvider(
 			get_class($this),
